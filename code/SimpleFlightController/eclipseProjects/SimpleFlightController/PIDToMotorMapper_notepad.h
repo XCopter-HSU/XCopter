dce0a498002eb6042b114645cb8ffaae96c71ef2
Thomas Weber <thweber@mail.hs-ulm.de>
2015-11-04 16:07:11 +0100

    changed motor[] to static

--- code/SimpleFlightController/eclipseProjects/SimpleFlightController/PIDToMotorMapper_notepad.h
+++ code/SimpleFlightController/eclipseProjects/SimpleFlightController/PIDToMotorMapper_notepad.h
@@ -5,8 +5,8 @@
 #include "Driver_Motor.h"
 #include "b_errorcodes.h"
 
-int8_t motorQuadx[4];
-int8_t motorQuadx[6];
+static int8_t motorQuadx[4];
+static int8_t motorQuadx[6];
 
 int8_t mapToMotors(int16_t throttle, int16_t roll, int16_t pitch, int16_t yaw, int16_t yawDirection);
 
